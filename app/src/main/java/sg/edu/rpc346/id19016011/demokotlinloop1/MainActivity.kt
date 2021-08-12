8df67280774dd048986aa802e2e681b9a97404ee
19016011 <19016011@rp.edu.sg>
2021-08-12 12:20:26 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rpc346/id19016011/demokotlinloop1/MainActivity.kt
@@ -0,0 +1,25 @@
+package sg.edu.rpc346.id19016011.demokotlinloop1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        button1.setOnClickListener {
+            for (i in 1..5) {
+                Log.d("MainActivity", "$i")
+            }
+        }
+        button2.setOnClickListener {
+            val word = editText.text.toString()
+
+            for (letter in word) {
+                Log.d("MainActivity", "$letter")
+            }
+        }
+    }
+}
